140a06316e9f1324cd450fc893b64a3de29c4552
ravillagopinath <ravilladhaneesh@gmail.com>
2024-10-13 00:04:39 +0530

    Add code to print all files

--- test.py
+++ test.py
@@ -1,15 +1,32 @@
 import os
 
+#local
+'''
+repo_path = os.getcwd()
+repo_name = 'dummy'
+'''
+
+
 print('----------------------')
 print('TEST GITHUB ACTIONS')
 print('----------------------')
 
 repo_name = os.environ.get('REPO_NAME')
 repo_path = os.environ.get('REPO_PATH')
+repo_url = os.environ.get('REPO_URL')
 print(repo_name)
 print('---------------------')
 print(repo_path)
 print('---------------------')
-
-
-
+print(repo_url)
+print('---------------------')
+    
+excluded_dirs = {'.git', '.github'}
+        
+for dirpath, dirnames, filenames in os.walk(repo_path):
+    dirnames[:] = [d for d in dirnames if d not in excluded_dirs]
+            
+    print(f"\nDirectory: {dirpath}")
+            
+    for file in filenames:
+        print(f"  File: {file}")
