30010e7ba6c90ac47c56f98b97250015e8ca2245
ravillagopinath <ravilladhaneesh@gmail.com>
2024-10-12 23:37:57 +0530

    Add repo url code

--- test.py
+++ test.py
@@ -5,5 +5,11 @@ print('TEST GITHUB ACTIONS')
 print('----------------------')
 
 repo_name = os.environ.get('REPO_NAME')
+repo_url = os.getcwd()
 print(repo_name)
 print('---------------------')
+print(repo_url)
+print('---------------------')
+
+
+
